b854cf00bf6e27848fe1e66dd1bf5f51d4972c3a
DEEPAK DAS <deepakdas.17@gmail.com>
2021-06-08 01:11:12 +0530

    Delete BankingResource.java

--- src/main/java/BankingResource.java
+++
@@ -1,8 +0,0 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BankingResource {
-	private static Logger logger = LoggerFactory.getLogger(BankingResource.class);
-}
